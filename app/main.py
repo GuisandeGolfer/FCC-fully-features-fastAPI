0b299596c4fea5c4e114ee70d07fc2ad2c53c3de
GuisandeGolfer <DGuisande873@gmail.com>
2024-07-02 17:37:29 -0700

    last push before new updated branch

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import post, user, auth, vote
+from routers import post, user, auth, vote
 # from .config import settings
 
 # Don't need anymore due to Alembic setup
