b7fd63412c1f24e8371894159014650930505765
GuisandeGolfer <DGuisande873@gmail.com>
2024-08-22 10:33:30 -0700

    getting ready to finish this project

--- app/main.py
+++ app/main.py
@@ -1,27 +1,13 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routers import post, user, auth, vote
 
 app = FastAPI()
 
-origins = [
-    "*"
-]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(post.router)
-app.include_router(user.router)
-app.include_router(auth.router)
-app.include_router(vote.router)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-@app.get('/')
-def read_root():
-    return {"message": "Welcome to my API!"}
+@app.get("/poopoo")
+async def toilet():
+    return {"action_needed": "FLUSH!! for gods sake flush!!"}
